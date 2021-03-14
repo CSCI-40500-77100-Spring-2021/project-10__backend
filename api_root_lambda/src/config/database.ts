6747a70ba9452268b0383d10c55574cd7d9e4be9
Asif Shikder <shikdernyc@gmail.com>
2021-03-13 21:18:47 -0500

    Added Stages

--- api_root_lambda/src/config/database.ts
+++ api_root_lambda/src/config/database.ts
@@ -5,79 +5,41 @@ import AppStage, { getAppStage } from '../constant/app_stage';
 
 dotenv.config();
 
-// Database Development Guide: https://csci435bookrec.atlassian.net/wiki/spaces/BRecSpace/pages/69664769/Database
 export interface DatabaseConfig {
-  Host: string
-  User: string
-  Password: string
   Dialect: Dialect
-  DatabaseName: string
+  ConnectionURL: string
 }
 
 class DefaultConfig implements DatabaseConfig {
-  Host: string;
-
-  User: string;
-
-  Password: string;
-
   Dialect: Dialect;
 
-  DatabaseName: string;
+  ConnectionURL: string;
 
   constructor() {
     const {
-      DB_HOST,
-      DB_USER,
-      DB_PASSWORD,
-      DB_NAME,
+      DB_URL,
     } = process.env;
-    if (DB_HOST === undefined) throw new Error('DB_HOST is not set as an env variable');
-    if (DB_USER === undefined) throw new Error('DB_USER is not set as an env variable');
-    if (DB_PASSWORD === undefined) throw new Error('DB_PASSWORD is not set as an env variable');
-    if (DB_NAME === undefined) throw new Error('DB_NAME is not set as an env variable');
-    this.Host = DB_HOST;
-    this.User = DB_USER;
-    this.Password = DB_PASSWORD;
+    if (DB_URL === undefined) throw new Error('Environment Variable Mission: DB_URL');
+
     this.Dialect = 'postgres';
-    this.DatabaseName = DB_NAME;
+    this.ConnectionURL = DB_URL;
   }
 }
 
-class TestConfig implements DatabaseConfig {
-  Host: string;
-
-  User: string;
-
-  Password: string;
-
-  Dialect: Dialect;
-
-  DatabaseName: string;
-
+class TestConfig extends DefaultConfig {
   constructor() {
-    // USER WILL NEED TO SETUP THIS DB IN THEIR OWN MACHINE
+    super();
     const {
-      TEST_DB_HOST,
-      TEST_DB_USER,
-      TEST_DB_PASSWORD,
-      TEST_DB_NAME,
+      TEST_DB_URL,
     } = process.env;
-    console.log(process.env);
-    if (TEST_DB_HOST === undefined) throw new Error('TEST_DB_HOST is not set as an env variable');
-    if (TEST_DB_USER === undefined) throw new Error('TEST_DB_USER is not set as an env variable');
-    if (TEST_DB_PASSWORD === undefined) throw new Error('TEST_DB_PASSWORD is not set as an env variable');
-    if (TEST_DB_NAME === undefined) throw new Error('TEST_DB_NAME is not set as an env variable');
-    this.Host = TEST_DB_HOST;
-    this.User = TEST_DB_USER;
-    this.Password = TEST_DB_PASSWORD;
-    this.Dialect = 'postgres';
-    this.DatabaseName = TEST_DB_NAME;
+    if (TEST_DB_URL === undefined) throw new Error('Environment Variable Mission: TEST_DB_URL');
   }
 }
 
 const getConfig = () => {
-  if (getAppStage() === AppStage.Test) return new TestConfig();
+  if (getAppStage() === AppStage.Test) {
+    return new TestConfig();
+  }
   return new DefaultConfig();
 };
 
--- cdk/src/index.ts
+++ cdk/src/index.ts
@@ -2,7 +2,15 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import AppStack from './stack/root_stack';
+import { getAppStage } from './constant/app_stage';
+
+const {
+  DB_URL,
+} = process.env;
+if (DB_URL === undefined) throw new Error('DB_URL is not set as an env variable');
 
 const app = new cdk.App();
 // eslint-disable-next-line no-new
-new AppStack(app, 'MealSnapAppStack');
+new AppStack(app, 'MealSnapAppStack', {
+  stage: getAppStage(),
+});
