5ca45017fedb0d5ea8f2bf43f51af309c06c724a
Asif Shikder <shikdernyc@gmail.com>
2021-04-30 00:36:45 -0400

    Added UserGalleryPhotoResponse model for templating responses for adding and retriving photos

---
+++ api_root_lambda/src/model/response/gallery.ts
@@ -0,0 +1,8 @@
+export interface UserGalleryPhotoResponse {
+  id: string,
+  title: string,
+  description: string,
+  imageUrl: string,
+  alreadyLiked: boolean,
+  likeCount: number
+}
--- api_root_lambda/src/router_handler/user_gallery/add_to_gallery.ts
+++ api_root_lambda/src/router_handler/user_gallery/add_to_gallery.ts
@@ -1,17 +1,30 @@
 import { RequestHandler } from 'express';
 import Gallery from '../../model/gallery';
+import { UserGalleryPhotoResponse } from '../../model/response/gallery';
+import { GalleryBucket } from '../../services/s3';
 import { GetCurrentUser } from '../../util/request';
+import GenerateId from '../../util/uuid';
 
 const AddToMyGalleryRequestHandler: RequestHandler = async (req, res, next) => {
   try {
     const { title, description, encodedImage } = req.body;
     const imageData = Buffer.from(encodedImage, 'base64');
+    const imageKey = `${GenerateId()}.jpg`;
+    const { imageUrl } = await GalleryBucket.UploadImage(imageKey, imageData);
     const imageSummary = await Gallery.AddToGallery(GetCurrentUser(), {
       title,
       description,
-      imageData,
+      imageUrl,
     });
-    return res.status(200).json(imageSummary);
+    const response : UserGalleryPhotoResponse = {
+      id: imageSummary.id,
+      title: imageSummary.title,
+      description: imageSummary.description,
+      imageUrl: imageSummary.imageUrl,
+      alreadyLiked: imageSummary.likedBy.has(GetCurrentUser()),
+      likeCount: imageSummary.likedBy.size,
+    };
+    return res.status(200).json(response);
   } catch (error) {
     return next(error);
   }
