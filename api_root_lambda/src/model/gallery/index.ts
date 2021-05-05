d7d585665b1607a87c7db3471bfbabdafef1191f
Asif Shikder <shikdernyc@gmail.com>
2021-05-05 18:11:02 -0400

    Sorting gallery photo in descending order

--- api_root_lambda/src/model/gallery/index.ts
+++ api_root_lambda/src/model/gallery/index.ts
@@ -86,6 +86,7 @@ export default class Gallery {
       },
       Limit: AppConfig.Preset.PaginationLimit,
       TableName: AppConfig.GalleryTableName,
+      ScanIndexForward: false,
     }).promise();
     // Parse Result
     if (result.Items === undefined) return { items: [] };
