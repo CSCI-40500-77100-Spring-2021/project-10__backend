2f865a4fb1b3da413a1da0ecaea2bbf2068a156e
Asif Shikder <shikdernyc@gmail.com>
2021-05-12 15:45:38 -0400

    Refactored upload code to use upload service

--- api_root_lambda/src/router_handler/user_gallery/add_to_gallery.ts
+++ api_root_lambda/src/router_handler/user_gallery/add_to_gallery.ts
@@ -1,16 +1,13 @@
 import { RequestHandler } from 'express';
 import Gallery from '../../model/gallery';
 import { UserGalleryPhotoResponse } from '../../model/response/gallery';
-import { GalleryBucket } from '../../services/s3';
+import UploadService from '../../services/upload_service';
 import { GetCurrentUser } from '../../util/request';
-import GenerateId from '../../util/uuid';
 
 const AddToMyGalleryRequestHandler: RequestHandler = async (req, res, next) => {
   try {
     const { title, description, encodedImage } = req.body;
-    const imageData = Buffer.from(encodedImage, 'base64');
-    const imageKey = `${GenerateId()}.jpg`;
-    const { imageUrl } = await GalleryBucket.UploadImage(imageKey, imageData);
+    const { previewUrl: imageUrl } = await UploadService.UploadImage(encodedImage);
     const imageSummary = await Gallery.AddToGallery(GetCurrentUser(req), {
       title,
       description,
