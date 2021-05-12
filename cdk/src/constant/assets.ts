51100a5e178b24b3b2d51c3baafb1d2cb856fbaa
Asif Shikder <shikdernyc@gmail.com>
2021-05-12 15:45:22 -0400

    Added Upload Handler Lambda Service to CDK

--- cdk/src/constant/assets.ts
+++ cdk/src/constant/assets.ts
@@ -3,3 +3,5 @@ import * as path from 'path';
 export const API_ROOT_LAMBDA_PATH = path.join(__dirname, '../../../api_root_lambda');
 
 export const USER_ADMIN_LAMBDA_PATH = path.join(__dirname, '../../../user_admin');
+
+export const UPLOAD_SERVICE_LAMBDA_PATH = path.join(__dirname, '../../../upload_handler');
